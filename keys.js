1a792e0403970e32ee2777bfe6262a08edc7c6d5
Whitney Tarkington <whittark@gmail.com>
2019-10-01 19:07:35 -0700

    Adding package files in GitHub due to specialized circumstances

---
+++ keys.js
@@ -0,0 +1,14 @@
+//console.log('this is loaded');
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'QlJH8KAzB0P7Qjfmbqh4AaVRu',
+  consumer_secret: 'ywU19riYRomctxOGZiPxElWsxOrEeCkg7EPJ4H9Yt3I8caNjMo',
+  access_token_key: '1083239561084383232-5HvjEuzWTa5C7nqBPWnppZ9lR45ae6',
+  access_token_secret: 'RgAF7TtZKy8ZfyRNL4VGUsj8SaWSzEYfr0ntKPSeCvRQ9',
+}
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
